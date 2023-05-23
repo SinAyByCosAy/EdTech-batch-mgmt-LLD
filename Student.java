ab259e157912cf5c89f79b574e1d163c0fac6516
TanaySrivas <tanaysriofficial@gmail.com>
2023-05-23 14:06:17 +0530

    Added getters and setters

--- Student.java
+++ Student.java
@@ -5,4 +5,30 @@ public class Student {
     private int age;
     private String batchId;
     private String batchName;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setBatchId(String batchId){
+        this.batchId = batchId;
+    }
+    public void setBatchName(String batchName){
+        this.batchName = batchName;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getBatchId(){
+        return batchId;
+    }
+    public String getBatchName(){
+        return batchName;
+    }
 }
