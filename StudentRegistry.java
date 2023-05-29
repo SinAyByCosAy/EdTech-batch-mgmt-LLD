8b8bcddb388004a7fb586a70300f56095c9560e2
TanaySrivas <tanaysriofficial@gmail.com>
2023-05-29 14:17:03 +0530

    Added Registry

---
+++ StudentRegistry.java
@@ -0,0 +1,23 @@
+package Prototype;
+
+import java.util.HashMap;
+
+public class StudentRegistry {
+    static StudentRegistry instance;
+    private StudentRegistry(){}
+    public static StudentRegistry getRegistryInstance(){
+        if(instance==null) {//not thread safe though
+            instance = new StudentRegistry();
+            return instance;
+        }
+        return instance;
+    }
+    HashMap<String, Student> registryValues = new HashMap<>();
+    void register(String key, Student s){
+        registryValues.put(key, s);
+    }
+
+    Student get(String key){
+        return registryValues.get(key);
+    }
+}
